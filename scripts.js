fa6dde4bc0293b72adfc4940a4ee6829286b9fbd
Ronak-08 <ronakameta65@gmail.com>
2024-06-23 20:14:51 +0530

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -148,30 +148,13 @@ document.getElementById('showColorPicker').addEventListener('click', function() 
     document.getElementById('colorPicker').click();
 });
 
-document.getElementById('colorPicker').addEventListener('input', function() {
-    let hexColor = this.value;
-    let rgbColor = hexToRgb(hexColor);
+
 
 document.getElementById('colorPicker').addEventListener('input', function() {
     let selectedColor = this.value;
     
-    document.documentElement.style.setProperty('--accent', rgbColor);
+    document.documentElement.style.setProperty('--accent', selectedColor);
     
-    document.documentElement.style.setProperty('--srcbar', rgbColor)
+    document.documentElement.style.setProperty('--srcbar', selectedColor)
 });
-
-
-// Function to convert hex to comma-separated RGB values
-function hexToRgb(hex) {
-    // Remove the hash at the start if it's there
-    hex = hex.replace(/^#/, '');
-    
-    // Parse r, g, b values
-    let bigint = parseInt(hex, 16);
-    let r = (bigint >> 16) & 255;
-    let g = (bigint >> 8) & 255;
-    let b = bigint & 255;
-
-    return `${r},${g},${b}`;
-}
 
