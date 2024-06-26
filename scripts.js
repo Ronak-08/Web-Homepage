7cb5391beac8aa656149ca2df8f5b141aad31e64
Ronak-08 <ronakameta65@gmail.com>
2024-06-26 13:36:46 +0530

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -242,7 +242,7 @@ document.addEventListener('DOMContentLoaded', function () {
 window.addEventListener('scroll', function() {
         var searchContainer = document.getElementById('searchContainer');
 
-        if (window.pageYOffset > 330) { // Adjust the value if needed
+        if (window.pageYOffset > 310) { // Adjust the value if needed
             searchContainer.classList.add('sticky');
         } else {
             searchContainer.classList.remove('sticky');
