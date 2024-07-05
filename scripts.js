6bbf53e5deeabca3b4f4e530efc92c0f197812a6
Ronak-08 <ronakameta65@gmail.com>
2024-07-05 17:05:27 +0530

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -484,4 +484,17 @@ const avatar = document.querySelector('.avatar');
         
         
     
- 
+ function openTab(evt, tabName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+    document.getElementById("defaultOpen").click();
